680483f89dc9709781fc6776dd29ea35f5ec5457
Kenneth Laurenz Franco <kenneth.franco@obf.ateneo.edu>
2021-07-25 16:28:32 +0800

    added landing page and gradle files

--- finalproj/app/src/main/java/com/example/myapplication/MainActivity.java
+++ finalproj/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @AfterViews
+    public void init(){
+
     }
 }
