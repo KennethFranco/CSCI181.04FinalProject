9279ec10c412c5697d30efee1db95faef4c17905
Kenneth Laurenz Franco <kenneth.franco@obf.ateneo.edu>
2021-07-25 16:31:42 +0800

    added customer button

--- finalproj/app/src/main/java/com/example/myapplication/MainActivity.java
+++ finalproj/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,16 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    @ViewById(R.id.customerButton)
+    Button customerB;
+
+
     @AfterViews
     public void init(){
 
+    }
+
+    @Click(R.id.customerButton)
+    public void customer(){
+        
     }
 }
