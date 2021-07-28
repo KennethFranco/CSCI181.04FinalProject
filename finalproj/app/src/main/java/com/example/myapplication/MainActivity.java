80ace81837b799a0cdbcc2c970b461036b1766c6
Marco Francisco <franciscomarco2921@gmail.com>
2021-07-29 00:23:14 +0800

    Added side drawer (not fully operational)

--- finalproj/app/src/main/java/com/example/myapplication/MainActivity.java
+++ finalproj/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -15,35 +24,70 @@ import org.androidannotations.annotations.ViewById;
 
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    @ViewById(R.id.customerButton)
-    Button customerB;
+//    @ViewById(R.id.customerButton)
+//    Button customerB;
+//
+//    @ViewById
+//    Button shopButton;
+//
+//    @ViewById(R.id.adminButton)
+//    Button admin;
 
-    @ViewById
-    Button shopButton;
-
-    @ViewById(R.id.adminButton)
-    Button admin;
-
+    private DrawerLayout drawer;
 
 
     @AfterViews
-    public void init(){
+    public void init() {
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 
-    @Click(R.id.customerButton)
-    public void customer(){
-        CustomerWelcome_.intent(this).start();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_customer:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
-    @Click(R.id.shopButton)
-    public void shopOwner() {
-        ShopOwnerWelcomePage_.intent(this).start();
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 
-    @Click(R.id.adminButton)
-    public void admin() {
-        AdminLogin_.intent(this).start();
-    }
+    //    @Click(R.id.customerButton)
+//    public void customer(){
+//        CustomerWelcome_.intent(this).start();
+//    }
+//
+//    @Click(R.id.shopButton)
+//    public void shopOwner() {
+//        ShopOwnerWelcomePage_.intent(this).start();
+//    }
+//
+//    @Click(R.id.adminButton)
+//    public void admin() {
+//        AdminLogin_.intent(this).start();
+//    }
 }
